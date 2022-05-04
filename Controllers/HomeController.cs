a9d465f73048423f5e141efe922dff84172f333f
kishan kumar <kishan.kumarmn@gmail.com>
2022-05-04 17:11:30 +0000

    Committed all files for FootBallLeague Database-first approach

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FootBallLeague;
+
+namespace FootBallLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        FIFAEntities context = new FIFAEntities(); //taken from context class
+        public ActionResult Index()
+        {
+
+            return View(context.FootBallLeagues.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
